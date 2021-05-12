349b508e60172f0e0905fce8bbc8d6db2bc7685a
Stanley Liu <sliu1993@hotmail.co.uk>
2021-05-12 17:08:46 +0100

    feat: allow file of URLs to be read into siege, and also separate operation modes of script into single-endpoint and file-read modes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # siege-tests
-Siege script for testing on our APIs
+
+Siege script for testing on our APIs. Run it via `bash ./siege.sh` after cloning it locally.
+
+Make sure you have Siege installed, either compiling like [their instructions](https://github.com/joedog/siege#installation)
+suggest, or through Homebrew (on MacOS), otherwise it will fail.
--- siege.sh
+++ siege.sh
@@ -8,81 +8,169 @@ then
   exit 1
 fi
 
-# TODO - allow multi-URL operation mode
 echo -e 'This is a simple bash script for running Siege on API endpoints. 
 You will need to supply: \n
  👉 An optional location to save test logs \n
+ 👉 The concurrent number of requests to make \n
+
+After that, you can either choose to run it in single-endpoint mode:
  👉 An endpoint \n
  👉 The verb, or type of request you are making \n
  👉 The body of the request \n
- 👉 The concurrent number of requests to make \n
+
+Or you can read in a text file of endpoints and verbs for each request. 
+It has to be in this format - and if you need any authentication for those endpoints
+in the form of query params, put them in there too: 
+# comments behind hashes
+http://homer.whoohoo.com/cgi-bin/hello.pl POST name=homer
+http://homer.whoohoo.com/haha.jsp POST word=doh!&scope=ALL
+
+# To post the contents of a file, use the redirect character <:
+http://www.haha.com/reader.php POST < /path/to/file
+
+# If you need authentication AND post file contents, its best to put the params in 
+# URL directly instead of separating them out
 '
 
-# Log location (optional)
-echo -n "Please enter the location where you want your test log to be saved or press enter to skip this and use the default location. 
-By default it will save to <location>/var where <location> is the directory in which Siege is installed.
-It will save it under the filename 'siege_test.log':"
-read -r log_location
 
-if [[ $log_location != '' ]] 
-then 
-  log_location="-l $log_location/siege_test.log"
-fi
+# Logging enabled (optional)
+echo -e "Enable logging? If so, logfile location will be outputted at the end of the test"
 
-# API endpoint (to test) 
-echo -n 'Please enter your endpoint:'
-
-# # Check for valid URLs using an ultra-sophisticated regex
-read -r endpoint 
-http_regex='(https|http)?://([^ ]+[.])+'
-
-while ! [[ $endpoint =~ $http_regex ]] ; do
-  echo -n "Please enter a valid URL. Make sure to include the protocol prefix: "
-  read -r endpoint
+select choice in "Y" "N"; do
+    case $choice in
+        Y ) 
+          enable_logging="-l" 
+          break;;
+        N )
+          enable_logging=""
+          break;;
+        *) 
+          echo "Select either 1 or 2 to proceed";;
+    esac
 done
 
-# Verb (GET, POST etc.)
-echo -n 'Please enter the HTTP verb:'
+# Mode functions
+single_endpoint_mode() {
+  # API endpoint (to test) 
+  echo -n 'Please enter your endpoint:'
 
-read verb
-valid_verbs=(GET PATCH POST PUT DELETE)
-uppercase_verb=$(echo "$verb" | tr a-z A-Z)
+  # Check for valid URLs using an ultra-sophisticated regex
+  read -r endpoint 
+  http_regex='(https|http)?://([^ ]+[.])+'
 
-while ! [[ " ${valid_verbs[@]} " =~ ${uppercase_verb} ]] ; do 
-  echo -n 'Please enter a valid HTTP verb:'
+  while ! [[ $endpoint =~ $http_regex ]] ; do
+    echo -n "Please enter a valid URL. Make sure to include the protocol prefix: "
+    read -r endpoint
+  done
+
+  # Verb (GET, POST etc.)
+  echo -n 'Please enter the HTTP verb:'
+
   read verb
-done
+  valid_verbs=(GET PATCH POST PUT DELETE)
+  uppercase_verb=$(echo "$verb" | tr a-z A-Z)
 
-# Body of the request in JSON format, e.g.
-# {  
-#    "coordinates":["11.180168326504514 43.92433128404457"],
-#    "auth_key":"O46KbWesN7FV5uGBVF8x",
-#    "auth_token":"o65avfsIU0YhcMGXfuqWk2TVg5ezJw",
-#    "type":"POINT",
-#    "buffer":"10000"
-#  } in oneline format
-echo -n 'Please provide the body of the request you wish to pass:' 
+  echo $uppercase_verb
+  while  [[ ! " ${valid_verbs[@]} " =~ ${uppercase_verb} ]] ; do 
+    echo -n 'Please enter a valid HTTP verb:'
+    read verb
+  done
 
-read -r body 
+  # Body of the request in JSON format, e.g.
+  # {  
+  #    "coordinates":["11.180168326504514 43.92433128404457"],
+  #    "auth_key":"O46KbWesN7FV5uGBVF8x",
+  #    "auth_token":"o65avfsIU0YhcMGXfuqWk2TVg5ezJw",
+  #    "type":"POINT",
+  #    "buffer":"10000"
+  #  } in oneline format
+  echo -n 'Please provide the body of the request you wish to pass:' 
+
+  read -r body 
 
 
-# Concurrent number of requests
-echo -n 'Please enter the concurrent number of requests you wish to make:'
+  # Concurrent number of requests
+  echo -n 'Please enter the concurrent number of requests you wish to make:'
 
-# Check for concurrency being an integer
-read concurrency
-number_regex='^[1-9]+$'
-
-while ! [[ $concurrency =~ $number_regex ]] ; do
-  echo -n "Please enter an integer value greater than 0:"
+  # Check for concurrency being an integer
   read concurrency
+  # TODO - fix
+  # number_regex='^[1-9]\d*$'
+
+  # while ! [[ $concurrency =~ $number_regex ]] ; do
+  #   echo -n "Please enter an integer value greater than 0:"
+  #   read concurrency
+  # done
+
+  # Run the Siege program
+  echo 'Running tests...'
+
+  eval "siege $enable_logging --concurrent=$concurrency --verbose --content-type='application/json' '$endpoint $uppercase_verb $body'"
+}
+
+file_read_mode() {
+  # File location 
+  echo -n 'Please enter the absolute path to your file:'
+  read -r file_location 
+
+  while ! [[ -f "$file_location" ]]; do
+    echo "File could not be found - did you provide the correct location?"
+    echo "Please try again: "
+    read -r file_location 
+  done
+
+  # Concurrent number of requests
+  echo -n 'Please enter the concurrent number of requests you wish to make:'
+
+  # Check for concurrency being an integer
+  read concurrency
+  # TODO - fix
+  # number_regex='^[1-9]+$'
+
+  # while ! [[ $concurrency =~ $number_regex ]] ; do
+  #   echo -n "Please enter an integer value greater than 0:"
+  #   read concurrency
+  # done
+  
+  # Run the Siege program
+  echo 'Running tests...'
+
+  eval "siege -f $file_location $enable_logging --concurrent=$concurrency --verbose --content-type='application/json'"
+}
+
+# There's two options to choose from - you can either provide a single endpoint or a file of endpoints to read from:
+echo -e 'Please select from the following options:'
+
+select selection in "Single endpoint mode" "File read mode" "Exit"; do
+    case $selection in
+      'Single endpoint mode' ) 
+        single_endpoint_mode
+        break;;
+      'File read mode' )
+        file_read_mode
+        break;;
+      'Exit' )
+        exit 0;;
+      *) 
+        echo "Select a valid option";;
+    esac
 done
-
-# Run the Siege program
-echo 'Running tests...'
-
-eval "siege $log_location --concurrent=$concurrency --verbose --content-type='application/json' '$endpoint $verb $body'"
 
 echo 'Test done!'
 
 exit 0
+
+# TODO - fix so we can run the script again
+# echo -e "Do you wish to run another test?"
+
+# select restart in "Y" "N"; do
+#     case $restart in
+#         Y ) 
+#           ./$(basename $0) && exit;;
+#         N )
+#           exit 0;;
+#         *) 
+#           echo "Select either 1 or 2 to proceed";;
+#     esac
+# done
+
